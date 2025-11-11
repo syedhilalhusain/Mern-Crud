85e05eb03a845ffcbd525db33e04f0ba6b1a75b8
Hilal Husain <147738830+syedhilalhusain@users.noreply.github.com>
2025-11-11 23:32:11 +0530

    Delete Realtime-Record_Master/Realtime-Record_Master directory

--- Realtime-Record_Master/Realtime-Record_Master/config/configs.js
+++
@@ -1,7 +0,0 @@
-// Set the connection string based from the config vars of the production server
-// To run locally use 'mongodb://localhost/mern-crud' instead of process.env.DB
-
-module.exports = {
-  db: process.env.DB,
-  react_app_url: "http://localhost:4200"
-};
